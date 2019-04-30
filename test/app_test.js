853bf4c4d0e8c1695f2d6e7f8a1cbb8e13f266b1
wildercossio <cwilrodz@gmail.com>
2019-04-29 21:29:34 -0400

    4ta prueba pasando

--- test/app_test.js
+++ test/app_test.js
@@ -11,8 +11,12 @@ describe('Guerreros',function(){
         let guerrero=new Infanteria("Erick");
         expect("Erick").equal(guerrero.nombre);
     });
-    it('Calcular el daño de arma para para un guerrero de tipo arquero que tiene solo arco',function(){
+    it('Calcular el daño de arma para un guerrero de tipo arquero que tiene solo arco',function(){
         let guerrero=new Arquero("Homero");
         expect(5).equal(guerrero.calcularDañoDeArma());
+    });
+    it('Calcular el daño de arma para un guerrero de tipo infanteria que tiene solo espada',function(){
+        let guerrero=new Infanteria("Waka");
+        expect(10).equal(guerrero.calcularDañoDeArma()); 
     });
 });
