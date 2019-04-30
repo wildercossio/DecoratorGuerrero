3087228ffab4ea421bd262b9df73c5c3363a5364
wildercossio <cwilrodz@gmail.com>
2019-04-29 20:46:18 -0400

    creada 2da prueba

--- test/app_test.js
+++ test/app_test.js
@@ -1,8 +1,13 @@
 import Guerrero from '../guerrero';
 var expect = require('chai').expect;
 describe('Guerreros',function(){
-    it('Devolver guerrero',function(){
+    it('Devolver el nombre del guerrero',function(){
         let guerrero=new Guerrero("Juan");
         expect("Juan").equal(guerrero.nombre);
     });
+    it('Devolver el nombre del guerrero de infanteria',function(){
+        let guerrero=new Infanteria("Erick");
+        expect("Erick").equal(guerrero.nombre);
+    });
+
 });
